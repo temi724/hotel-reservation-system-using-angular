629c4a65447a78392c190c0f81b2a07bf37867eb
DANIEL TEMITAYO PETER <tayodaniel0@gmail.com>
2025-04-15 00:17:28 +0100

    Changes to be committed: 	modified:   hotel-app/src/app/app-routing.module.ts 	modified:   hotel-app/src/app/app.module.ts 	modified:   hotel-app/src/app/home/home.component.html 	modified:   hotel-app/src/app/home/home.module.ts

--- hotel-app/src/app/app.module.ts
+++ hotel-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReservationModule } from './reservation/reservation.module';
+import { HomeModule } from './home/home.module';
 // import { ReservationFormComponent } from './reservation-form/reservation-form.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReservationModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
