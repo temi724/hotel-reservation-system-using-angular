8694b27454d65517184a7978c9599473b3a6396d
DANIEL TEMITAYO PETER <tayodaniel0@gmail.com>
2025-04-16 01:03:19 +0100

    Changes to be committed: 	modified:   hotel-app/src/app/home/home.component.html 	modified:   hotel-app/src/app/home/home.component.ts 	modified:   hotel-app/src/app/home/home.module.ts 	modified:   hotel-app/src/app/reservation-form/reservation-form.component.html 	modified:   hotel-app/src/app/reservation-list/reservation-list.component.html 	modified:   hotel-app/src/app/reservation/reservation.module.ts

--- hotel-app/src/app/reservation/reservation.module.ts
+++ hotel-app/src/app/reservation/reservation.module.ts
@@ -4,9 +4,16 @@ import { ReservationListComponent } from '../reservation-list/reservation-list.c
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HomeModule } from '../home/home.module';
 
 @NgModule({
   declarations: [ReservationListComponent, ReservationFormComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HomeModule,
+  ],
 })
 export class ReservationModule {}
